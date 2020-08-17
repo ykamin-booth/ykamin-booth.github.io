83c5b405d82df4a62050f76f75954093a7c6c268
Ubuntu <ubuntu@ip-172-31-38-235.us-east-2.compute.internal>
2020-08-17 21:58:25 +0000

    finish the JS gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,31 @@
+//function activateGallery() {
+ // alert("Hello from the gallery file!");
+//}
 function activateGallery() {
-  alert("Hello from the gallery file!");
+let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+let mainImage = document.querySelector("#gallery-photo > img");
+let currentClass="current";
+let mainImageTitle = document.querySelector("#gallery-info > .title");
+let mainImageDesc = document.querySelector("#gallery-info > .description");
+
+thumbnails.forEach(function(thumbnail) {
+  thumbnail.addEventListener("click", function() {
+    let newImageSrc = thumbnail.dataset.largeVersion;
+    let newImageAlt = thumbnail.alt;
+    let newImageTitle = thumbnail.dataset.title;
+    let newImageDesc = thumbnail.dataset.description;
+    
+    //change main image
+    mainImage.setAttribute("src", newImageSrc);
+    mainImage.setAttribute("alt",newImageAlt);
+    
+    //change current image in sidebar
+    document.querySelector(`.${currentClass}`).classList.remove(currentClass);
+    thumbnail.parentNode.classList.add(currentClass);
+    
+    //change title and description
+    mainImageTitle.innerHTML = newImageTitle;
+    mainImageDesc.innerHTML = newImageDesc;
+    });
+});
 }
